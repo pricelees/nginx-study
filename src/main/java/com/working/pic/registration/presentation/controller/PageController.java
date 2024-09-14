ade2cef4be3bd269f20d5690a955b0a2374ac69b
pricelees <priceelees@gmail.com>
2024-09-15 02:11:20 +0900

    feat: add PageController to handle registration page

---
+++ src/main/java/com/working/pic/registration/presentation/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.working.pic.registration.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+	@GetMapping("/register")
+	public String register() {
+		return "registration";
+	}
+}
