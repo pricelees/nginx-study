b8dca5f470aca18f90b8bf07c0d7580d3be04263
pricelees <priceelees@gmail.com>
2024-09-15 16:59:01 +0900

    feat: add sample Docker setup file

---
+++ docker/create-compose-with-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source docker-var.sh
+
+## insert springboot entrypoint_template var
+export SPRING_V1_ENTRYPOINT=$(./create-entrypoint.sh template/entrypoint_template)
+export SPRING_V2_ENTRYPOINT=$(./create-entrypoint.sh template/entrypoint_v2_template)
+
+envsubst < springboot-compose-format > docker-compose-springboot.yml
+envsubst < mysql-compose-format > docker-compose-mysql.yml
+
+## create docker network if not exists
+if [ -z "$(docker network ls --filter name=^${DOCKER_NETWORK} --format '{{ .Name }}')" ]; then
+    docker network create $DOCKER_NETWORK
+fi
+
+# create mysql volume if not exists
+if [ -z "$(docker volume ls --filter name=^${MYSQL_DOCKER_VOLUME} --format '{{ .Name }}')" ]; then
+    docker volume create $MYSQL_DOCKER_VOLUME
+fi
