200de5932f5b4f456cf2056811eb058252606462
pricelees <priceelees@gmail.com>
2024-09-15 21:18:21 +0900

    refactor: remove port in headers page

--- src/main/java/com/working/pic/registration/presentation/controller/PageController.java
+++ src/main/java/com/working/pic/registration/presentation/controller/PageController.java
@@ -26,16 +26,12 @@ public class PageController {
 
 	private String getHeaderPage(HttpServletRequest request, Model model) {
 		String scheme = request.getScheme();
-		int remotePort = request.getRemotePort();
-		int serverPort = request.getServerPort();
 		String remoteHost = request.getRemoteHost();
-		StringBuffer requestURL = request.getRequestURL();
+		String requestURL = request.getRequestURL().toString();
 		String remoteAddr = request.getRemoteAddr();
 		model.addAttribute("scheme", scheme);
-		model.addAttribute("remotePort", remotePort);
-		model.addAttribute("serverPort", serverPort);
 		model.addAttribute("remoteHost", remoteHost);
-		model.addAttribute("requestUrl", requestURL.toString());
+		model.addAttribute("requestUrl", requestURL);
 		model.addAttribute("remoteAddr", remoteAddr);
 
 		return "headers";
