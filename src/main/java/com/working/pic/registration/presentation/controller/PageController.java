d50eab08c85b9874606b135ab869b3961eb4a3f0
이상진 <priceelees@gmail.com>
2024-09-17 20:26:06 +0900

    IP 주소 확인 방법 수정 및 X-Forwarded-For 헤더 표시 추가 (#12)
    * refactor: return only the first two segments of the IP Address
    
    * feat: add x-forwarded-header value on headers page
    
    * fix: center alignment on mobile page
    
    * add log for debug
    
    * fix: Thymeleaf display for X-Forwarded-For header
    
    - Updated Thymeleaf template to display 'null' when X-Forwarded-For is null
    - Shows actual value when X-Forwarded-For is not null

--- src/main/java/com/working/pic/registration/presentation/controller/PageController.java
+++ src/main/java/com/working/pic/registration/presentation/controller/PageController.java
@@ -1,12 +1,17 @@
 package com.working.pic.registration.presentation.controller;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class PageController {
 
 	@GetMapping("/register")
@@ -25,15 +30,36 @@ public class PageController {
 	}
 
 	private String getHeaderPage(HttpServletRequest request, Model model) {
-		String scheme = request.getScheme();
-		String remoteHost = request.getRemoteHost();
-		String requestURL = request.getRequestURL().toString();
-		String remoteAddr = request.getRemoteAddr();
-		model.addAttribute("scheme", scheme);
-		model.addAttribute("remoteHost", remoteHost);
-		model.addAttribute("requestUrl", requestURL);
-		model.addAttribute("remoteAddr", remoteAddr);
+		model.addAttribute("scheme", request.getScheme());
+		model.addAttribute("requestUrl", request.getRequestURL().toString());
+		model.addAttribute("remoteAddr", parseIpAddress(request.getRemoteAddr()));
+		addXForwardedForHeaderIfExist(request, model);
 
 		return "headers";
+	}
+
+	private void addXForwardedForHeaderIfExist(HttpServletRequest request, Model model) {
+		String xForwardedForHeader = request.getHeader("X-Forwarded-For");
+		log.info("request: {}, X-Forwarded-For: {}", request.getRequestURI(), xForwardedForHeader);
+		if (xForwardedForHeader == null) {
+			return;
+		}
+
+		model.addAttribute("xForwardedFor", parseXForwardedForHeader(xForwardedForHeader));
+	}
+
+	private String parseIpAddress(String ipAddress) {
+		String[] parts = ipAddress.split("\\.");
+		if (parts.length == 1) {
+			parts = ipAddress.split(":");
+		}
+		return parts[0] + "." + parts[1];
+	}
+
+	private String parseXForwardedForHeader(String header) {
+		String[] parts = header.split(", ");
+		return Arrays.stream(parts)
+			.map(this::parseIpAddress)
+			.collect(Collectors.joining(", "));
 	}
 }
