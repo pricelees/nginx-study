bb2b39b196ff232e8edeae4b01a0227a7fb19d99
pricelees <priceelees@gmail.com>
2024-09-17 18:04:08 +0900

    feat: add x-forwarded-header value on headers page

--- src/main/java/com/working/pic/registration/presentation/controller/PageController.java
+++ src/main/java/com/working/pic/registration/presentation/controller/PageController.java
@@ -1,5 +1,8 @@
 package com.working.pic.registration.presentation.controller;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +31,18 @@ public class PageController {
 		model.addAttribute("scheme", request.getScheme());
 		model.addAttribute("requestUrl", request.getRequestURL().toString());
 		model.addAttribute("remoteAddr", parseIpAddress(request.getRemoteAddr()));
+		addXForwardedForHeaderIfExist(request, model);
 
 		return "headers";
+	}
+
+	private void addXForwardedForHeaderIfExist(HttpServletRequest request, Model model) {
+		String xForwardedForHeader = request.getHeader("X-Forwarded-For");
+		if (xForwardedForHeader == null) {
+			return;
+		}
+
+		model.addAttribute("xForwardedFor", parseXForwardedForHeader(xForwardedForHeader));
 	}
 
 	private String parseIpAddress(String ipAddress) {
@@ -38,5 +51,12 @@ public class PageController {
 			parts = ipAddress.split(":");
 		}
 		return parts[0] + "." + parts[1];
+	}
+
+	private String parseXForwardedForHeader(String header) {
+		String[] parts = header.split(", ");
+		return Arrays.stream(parts)
+			.map(this::parseIpAddress)
+			.collect(Collectors.joining(", "));
 	}
 }
