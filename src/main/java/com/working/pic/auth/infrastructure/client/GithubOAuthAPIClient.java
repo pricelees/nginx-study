d4364f854560c4fbe3497bafb89414b14762f6dd
pricelees <priceelees@gmail.com>
2024-09-19 01:16:40 +0900

    Revert "refactor: resize font from <p> to <h3> for better readability"
    This reverts commit 339aeca8dfe63ca14e414e8628534b77c33085aa.

--- src/main/java/com/working/pic/auth/infrastructure/client/GithubOAuthAPIClient.java
+++ src/main/java/com/working/pic/auth/infrastructure/client/GithubOAuthAPIClient.java
@@ -1,25 +1,18 @@
 package com.working.pic.auth.infrastructure.client;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 import com.working.pic.auth.infrastructure.dto.GithubUserDto;
 import com.working.pic.auth.infrastructure.dto.GithubUserEmailDto;
 
-import lombok.extern.slf4j.Slf4j;
-
 @EnableConfigurationProperties(GithubOAuthProperties.class)
 @Component
-@Slf4j
 public class GithubOAuthAPIClient {
 
 	private static final String DEFAULT_RESPONSE_CONTENT_TYPE = "application/vnd.github+json";
@@ -48,21 +41,10 @@ public class GithubOAuthAPIClient {
 			.uri(properties.getApiUrl().getEmails())
 			.header("Authorization", "Bearer " + accessToken)
 			.retrieve()
-			.onStatus(this::isGitHubApiError, this::handleError)
-			.body(new ParameterizedTypeReference<>() {});
+			.body(new ParameterizedTypeReference<>() {
+			});
 
-		assert results != null;
 		return getPrimaryEmail(results);
-	}
-
-	private boolean isGitHubApiError(HttpStatusCode status) {
-		return status.is4xxClientError() || status.is5xxServerError();
-	}
-
-	private void handleError(HttpRequest request, ClientHttpResponse response) throws IOException {
-		log.error("Failed to get email From Github API. status: {}, body: {}", response.getStatusCode(),
-			response.getBody());
-		throw new IllegalStateException("Failed to get email from Github API");
 	}
 
 	private GithubUserEmailDto getPrimaryEmail(List<GithubUserEmailDto> results) {
