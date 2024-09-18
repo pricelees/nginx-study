b65e5141cf729cc2943305a855d141530d679d77
이상진 <priceelees@gmail.com>
2024-09-19 01:23:55 +0900

    도커와 localhost IP는 그대로 표시하도록 수정 (#14)
    * refactor: display ip as is if it is docker-ip and extract into HeaderPagecontroller class
    
    * refactor: remove scheme, requesturl on headers page
    
    * comment: add comment on getHeaderPage() method
    
    * refactor: HeaderPageController for improved readability
    
    * refactor: change page titles
    
    * refactor: resize font from <p> to <h3> for better readability
    
    * refactor: resize font from <p> to <h3> for better readability
    
    * Revert "refactor: resize font from <p> to <h3> for better readability"
    
    This reverts commit 339aeca8dfe63ca14e414e8628534b77c33085aa.
    
    * refactor: resize font from <p> to <h3> for better readability
    
    * refactor: restore font size to original in headers.html

---
+++ src/main/java/com/working/pic/HeaderPageController.java
@@ -0,0 +1,93 @@
+package com.working.pic;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HeaderPageController {
+
+	private static final String DOCKER_NETWORK_SUBNET = "172.18";
+	private static final String LOCALHOST_IP_PREFIX = "127.0";
+	private static final String X_FORWARDED_FOR_HEADER_DELIMITER = ", ";
+	private	static final String IPv4_SEPARATOR_REGEX = "\\.";
+	private static final String IPv4_SEPARATOR = ".";
+	private static final String IPv6_SEPARATOR = ":";
+
+	@GetMapping("/v1/headers")
+	public String v1Headers(HttpServletRequest request, Model model) {
+		return getHeaderPage(request, model);
+	}
+
+	@GetMapping("/v2/headers")
+	public String v2Headers(HttpServletRequest request, Model model) {
+		return getHeaderPage(request, model);
+	}
+
+	/**
+	 * 클라이언트 IP 정보를 받아 모델에 추가한 후, headers.html 페이지를 반환합니다.
+	 *
+	 * @param request HttpServletRequest 객체로, 클라이언트의 요청 정보를 담고 있습니다.
+	 * @param model Model 객체로, 뷰에 전달할 데이터를 담습니다.
+	 * @return 클라이언트의 IP 주소(request.getRemoteAddr())와
+	 *         X-Forwarded-For 헤더(request.getHeader("X-Forwarded-For")) 값을
+	 *         모델에 추가하고, headers.html 페이지를 반환합니다.
+	 */
+	private String getHeaderPage(HttpServletRequest request, Model model) {
+		model.addAttribute("remoteAddr", parseIpAddress(request.getRemoteAddr()));
+		addXForwardedForHeaderIfExist(request, model);
+
+		return "headers";
+	}
+
+	private void addXForwardedForHeaderIfExist(HttpServletRequest request, Model model) {
+		String xForwardedForHeader = request.getHeader("X-Forwarded-For");
+		log.info("request: {}, X-Forwarded-For: {}", request.getRequestURI(), xForwardedForHeader);
+
+		if (xForwardedForHeader == null) {
+			return;
+		}
+
+		model.addAttribute("xForwardedFor", parseXForwardedForHeader(xForwardedForHeader));
+	}
+
+	private String parseXForwardedForHeader(String header) {
+		String[] parts = header.split(X_FORWARDED_FOR_HEADER_DELIMITER);
+		return Arrays.stream(parts)
+			.map(this::parseIpAddress)
+			.collect(Collectors.joining(X_FORWARDED_FOR_HEADER_DELIMITER));
+	}
+
+	private String parseIpAddress(String ipAddress) {
+		if (isDockerOrLocalhostIPAddress(ipAddress)) {
+			return ipAddress;
+		}
+
+		return getFirstTwoSegments(ipAddress);
+	}
+
+	private boolean isDockerOrLocalhostIPAddress(String ipAddress) {
+		return ipAddress.startsWith(DOCKER_NETWORK_SUBNET) || ipAddress.startsWith(LOCALHOST_IP_PREFIX);
+	}
+
+	private String getFirstTwoSegments(String ipAddress) {
+		if (isIpv6Address(ipAddress)) {
+			String[] segments = ipAddress.split(IPv6_SEPARATOR);
+			return segments[0] + IPv6_SEPARATOR + segments[1];
+		}
+
+		String[] segments = ipAddress.split(IPv4_SEPARATOR_REGEX);
+		return segments[0] + IPv4_SEPARATOR + segments[1];
+	}
+
+	private boolean isIpv6Address(String ipAddress) {
+		return ipAddress.contains(IPv6_SEPARATOR);
+	}
+}
--- src/main/java/com/working/pic/registration/presentation/controller/PageController.java
+++ src/main/java/com/working/pic/registration/presentation/controller/PageController.java
@@ -1,13 +1,8 @@
 package com.working.pic.registration.presentation.controller;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -17,49 +12,5 @@ public class PageController {
 	@GetMapping("/register")
 	public String register() {
 		return "registration";
-	}
-
-	@GetMapping("/v1/headers")
-	public String v1Headers(HttpServletRequest request, Model model) {
-		return getHeaderPage(request, model);
-	}
-
-	@GetMapping("/v2/headers")
-	public String v2Headers(HttpServletRequest request, Model model) {
-		return getHeaderPage(request, model);
-	}
-
-	private String getHeaderPage(HttpServletRequest request, Model model) {
-		model.addAttribute("scheme", request.getScheme());
-		model.addAttribute("requestUrl", request.getRequestURL().toString());
-		model.addAttribute("remoteAddr", parseIpAddress(request.getRemoteAddr()));
-		addXForwardedForHeaderIfExist(request, model);
-
-		return "headers";
-	}
-
-	private void addXForwardedForHeaderIfExist(HttpServletRequest request, Model model) {
-		String xForwardedForHeader = request.getHeader("X-Forwarded-For");
-		log.info("request: {}, X-Forwarded-For: {}", request.getRequestURI(), xForwardedForHeader);
-		if (xForwardedForHeader == null) {
-			return;
-		}
-
-		model.addAttribute("xForwardedFor", parseXForwardedForHeader(xForwardedForHeader));
-	}
-
-	private String parseIpAddress(String ipAddress) {
-		String[] parts = ipAddress.split("\\.");
-		if (parts.length == 1) {
-			parts = ipAddress.split(":");
-		}
-		return parts[0] + "." + parts[1];
-	}
-
-	private String parseXForwardedForHeader(String header) {
-		String[] parts = header.split(", ");
-		return Arrays.stream(parts)
-			.map(this::parseIpAddress)
-			.collect(Collectors.joining(", "));
 	}
 }
